9aa607de466540681ba2cbe0380d2b0376b0a7e8
Omar Valencia <omarvalencia9415@gmail.com>
2020-12-17 10:38:14 -0500

    initial commit

---
+++ js/operacionesMat.js
@@ -0,0 +1,25 @@
+function sumar() {
+    //Recibir los datos
+    let numeroU = parseInt(document.getElementById("nUno").value)
+    let numeroD = parseInt(document.getElementById("nDos").value)
+
+    //Procesar los datos
+    let resultado = parseInt(numeroU + numeroD)
+
+    //Entregar los resultados
+    document.getElementById("resulOperacion").innerHTML = resultado
+    
+}
+
+function multiplicar() {
+    //Recibir los datos
+    let numeroU = parseInt(document.getElementById("nUno").value)
+    let numeroD = parseInt(document.getElementById("nDos").value)
+
+    //Procesar los datos
+    let resultado = parseInt(numeroU * numeroD)
+
+    //Entregar los resultados
+    document.getElementById("resulOperacion").innerHTML = resultado
+    
+}
